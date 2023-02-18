e0341a6dfb225d990219b5f2dc463d042051739f
JhonEdisonPinto <124928317+JhonEdisonPinto@users.noreply.github.com>
2023-02-18 12:41:44 -0500

    Tarea
    taller java

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,48 @@
+package test;
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento [] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	int cantidadAsientos()
+	{
+		int canAsientos=0;
+		for (int i = 0 ; i < asientos.length; i++)
+		{ 
+			if (asientos[i] != null)
+			{
+				canAsientos++;
+			}
+		}
+		return canAsientos;
+	}
+	
+	String verificarIntegridad()
+	{
+		if (registro == motor.registro)
+		{
+			for(int i = 0; i <asientos.length; i++)
+			{
+				if (asientos[i].registro != registro)
+				{
+					return"Las piezas no son originales";
+				}
+
+				
+			}
+		return"Auto original";
+	
+		}else {
+			return "Las piezas no son originales";
+		}
+	}
+}
+	
+	
+	
+
+
